d5b884fc2f2c2c3a5bacc192a891788769b48f57
Ashu398 <chaubeyashutosh980@gmail.com>
2024-09-10 18:49:59 +0530

    changed config file name

--- src/main/java/com/demo/Ashututorial/tutorial/configuration/AppConfig.java
+++ src/main/java/com/demo/Ashututorial/tutorial/configuration/AppConfig.java
@@ -6,11 +6,10 @@ import com.demo.Ashututorial.tutorial.devDB;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Appconfi {
+public class AppConfig {
     @Bean
     @ConditionalOnProperty(name="project.mode",havingValue="development")
     public DB getDEVdbBean() {
