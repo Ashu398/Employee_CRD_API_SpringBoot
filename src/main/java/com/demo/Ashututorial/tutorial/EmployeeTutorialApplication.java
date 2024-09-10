9c1d9b9337a4d9be3cdc9df3132577db3ed02d39
Ashu398 <chaubeyashutosh980@gmail.com>
2024-09-10 19:05:27 +0530

    changed file name

--- src/main/java/com/demo/Ashututorial/tutorial/EmployeeTutorialApplication.java
+++ src/main/java/com/demo/Ashututorial/tutorial/EmployeeTutorialApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class AshuTutorialApplication implements CommandLineRunner {
+public class EmployeeTutorialApplication implements CommandLineRunner {
 	@Autowired
 	DB db;
 	public static void main(String[] args) {
-		SpringApplication.run(AshuTutorialApplication.class, args);}
+		SpringApplication.run(EmployeeTutorialApplication.class, args);}
 	@Override
 	public void run(String... args) throws Exception {
 
